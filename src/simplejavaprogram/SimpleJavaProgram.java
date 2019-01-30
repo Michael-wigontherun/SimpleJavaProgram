c8b00da8f662f4a324c079a0ed92eb6dcb3af440
Michael.ftt@outlook.com <>
2019-01-30 12:34:59 -0500

    first commit

---
+++ src/simplejavaprogram/SimpleJavaProgram.java
@@ -0,0 +1,11 @@
+package simplejavaprogram;
+
+public class SimpleJavaProgram {
+    public static void main(String[] args) {
+        Flett();
+    }
+    private static void Flett() {
+        System.out.println("Michael Flett");
+    }
+    
+}
